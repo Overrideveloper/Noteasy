14d93625aa68e5c076a7ffb30434df7f2f26f20c
Banso D. Wisdom <bansowisdom@gmail.com>
2017-08-31 00:36:33 +0100

    Update webconfig.php

--- app/config/webconfig.php
+++ app/config/webconfig.php
@@ -1,16 +1,11 @@
 <?php
     //
     /*Defining keys to be used in connection string
-    define('server', 'override.dev.com');
-    define('username', 'root');
-    define('password', 'alleuname');
-    define('database', 'notepad');
-    */
 
-    $host ='localhost';
-    $username ='root';
-    $password='alleuname';
-    $db = 'notepad';
+    $host ='*****';
+    $username ='*****';
+    $password='******';
+    $db = '******';
 
     //
     //Database connection like connection strings in ASP.NET
